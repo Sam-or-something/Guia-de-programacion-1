fb11951c2e7061933b095f876a8101e6feb5616b
Sam <89226291+Sam-or-something@users.noreply.github.com>
2022-04-05 10:14:48 -0300

    Exercise 6 complete

---
+++ Assets/Scripts/Ej6.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ej6 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    //Realizá un programa que permita el ingreso por Inspector de un valor entero mayor que 0
+    //en una variable llamada num1 y muestre un mensaje por pantalla indicando "el número es" o "el número es impar". 
+    //Deberá utilizar el operador “módulo” es el caracter %.
+
+    public int num1;
+
+    void Start()
+    {
+        if (num1 <= 0)
+        {
+            Debug.Log("el numero debe ser mayor a 0");
+        }
+        else if (num1 % 2 == 1)
+        {
+            Debug.Log("el numero es impar");
+        }
+        else
+        {
+            Debug.Log("el numero es par");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
